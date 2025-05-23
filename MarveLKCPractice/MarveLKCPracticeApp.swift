d976f24a0e5cb8e2d03a5dc6504edade0e09c66a
GreibisFarias <153131030+GreibisFarias@users.noreply.github.com>
2025-05-22 23:10:28 -0500

    Initial Commit

---
+++ MarveLKCPractice/MarveLKCPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarveLKCPracticeApp.swift
+//  MarveLKCPractice
+//
+//  Created by Greibis Farias on 5/22/25.
+//
+
+import SwiftUI
+
+@main
+struct MarveLKCPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
